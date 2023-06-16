91980cdb479a2686d280407a331f0fdb60c6b8e0
ejim005 <eunjin@4thevision.com>
2023-06-16 23:56:57 +0900

    리액트 프로그램구조 및 이벤트핸들링

--- study/src/App.js
+++ study/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
+
+    const onSubmit = () => {
+        alert('hello3');
+    }
+
+    const onKeyUp = (event) => {
+        console.log('key up');
+        if(event.keyCode === 13) {
+            onSubmit();
+        }
+    }
+
+    function onSubmit2() {
+        alert('hello4');
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={()=>alert('hello')}>Summit</button>
+        <button onClick={alert('hello2')}>Summit2</button> {/*렌더링할때 바로실행되고 버튼누르면 동작안함*/}
+        <button onClick={onSubmit}>Summit3</button>
+        <button onClick={onSubmit2()}>Summit4</button> {/*렌더링할때 바로실행되고 버튼누르면 동작안함*/}
+
+        <input onKeyUp={onKeyUp}/>
     </div>
   );
 }

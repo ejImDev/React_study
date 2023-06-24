340e9b4e86d15b6e2d36a6205359106e2ebe478e
ejImDev <ejim0093@gmail.com>
2023-06-24 23:43:29 +0900

    useEffect 사용

--- study/src/App.js
+++ study/src/App.js
@@ -1,29 +1,34 @@
-import React, { useState} from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-    const[username, setUsername] = useState('');
-    const[password, setPassword] = useState('');
-    const onSubmit = () => {
-        alert('submitted');
-        console.log(username)
-        console.log(password)
+
+    const [count, setCount] = useState(0);
+    const [im, setIm] = useState(0);
+
+    useEffect(() => {
+        console.log(count)
+    }, [count])
+    /*useEffect가 렌더링 될 때 마다 실행 됨*/
+    /*[count]를 넣어주면 im이 변경될때는 실행안되고 count가 변경될때만 실행됨*/
+
+    useEffect(() => {
+        console.log(im)
+    }, [im])
+
+    useEffect(() => {
+        console.log('first rendering')
+    }, []);
+    /*[]를 넣어주면 첫 렌더링때만 실행됨*/
+
+    const increment = () => {
+        setCount(count + 1);
     }
 
     return (
     <div className="App">
-       <form onSubmit={onSubmit}>
-        <input
-           placeholder="Username"
-           value={username}
-           onChange={(e) => setUsername(e.target.value)}
-       /><br />
-        <input
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        /><br />
-        <button type="submit">Login</button>
-       </form>
+        <h1>Text</h1>
+        <button onClick={increment}>Click</button>
+        <button onClick={() => setIm(im + 1)}>Click1</button>
     </div>
   );
 }

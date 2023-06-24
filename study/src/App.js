42491b671e14facc1a90305e69c2eb9e8c1e1aa7
ejImDev <ejim0093@gmail.com>
2023-06-25 02:29:37 +0900

    부모컴포넌트에서 자식컴포넌트로 데이터 전달

--- study/src/App.js
+++ study/src/App.js
@@ -1,44 +1,33 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
+/*
+다른 컴포넌트를 호출해서 사용하는 컴포넌트 = 부모 컴포넌트
+호출되어지는 컴포넌트를 자식 컴포넌트라고 함
+
+여기서는 App이 부모, Counter가 자식 컴포넌트가 되는 것
+*/
 
 function App() {
 
-    /*
-    const [count, setCount] = useState(0);
-    const [count2, setCount2] = useState(0);
-    const [count3, setCount3] = useState(0);
-
-    const increment = () => {
-        setCount(count + 1);
-    }
-    const increment2 = () => {
-        setCount2(count2 + 1);
-    }
-    const increment3 = () => {
-        setCount3(count3 + 1);
-    }
-    */
-    /*
-    위처럼 구현하는 경우 각각
-    다른 버튼으로 사용해서 기능을 구현할수는 있으나
-    불필요하게 반복되는 코드들이 길어짐.
-    또 똑같은 내용으로 수정하려할때 하나하나 찾아서 바꿔줘야함.
-    따라서 컴포넌트를 사용
-    */
+    const [buttonName, setButtonName] = useState('클릭');
+    /*상태를 보내는 것도 가능함*/
+    const clickButton = () => {
+        setButtonName('click')
+    };
+    /* state가 변경되면 자식컴포넌트로 전달되는 데이터도 변경됨*/
 
     return (
     <div className="App">
         <h1>Text</h1>
-        {/*
-        <button onClick={increment}>Click {count}</button>
-        <button onClick={increment2}>Click {count2}</button>
-        <button onClick={increment3}>Click {count3}</button>
-        */}
-        {/* 각각으로 받아오던거 지우고 컴포넌트를 호출 */}
+        <Counter click="click1" />
+        <Counter click={buttonName}/>
         <Counter />
-        <Counter />
-        <Counter />
-        {/* 컴포넌트 호출을 하면 각각의 상태로 관리 됨 */}
+        {/* 컴포넌트를 호출하면서 속성으로 데이터를 전송해줄수 있음.
+        여기서는 click이라는 이름으로 각 값을 컴포넌트에 전송한것이 됨 */}
+
+        <br />
+        <button onClick={clickButton}>상태 변경</button>
+
     </div>
   );
 }
--- study/src/components/Counter.js
+++ study/src/components/Counter.js
@@ -1,13 +1,22 @@
 import React, {useState} from "react";
 
-const Counter = () => {
+const Counter = (props) => {
+    /* 자식컴포넌트에서는 props를 사용하여 부모컴포넌트에서 전송한 데이터를 받음
+    * 부모컴포넌트에서 속성에 지정한 키를 사용해 값을 호출 가능 */
+
     const [count, setCount] = useState(0);
     const increment = () => {
         setCount(count + 2);
     }
 
+    const clickString = props.click || 'Click';
+    /* 전달된 데이터가 없을 경우 디폴트로 사용할 내용을 지정할 수 있음 */
+
     return (
-        <button onClick={increment}>Click {count}</button>
+        /*<button onClick={increment}>{props.click} {count}</button>*/
+        /* 바로 전달객체를 사용하려면 {props.전달한 키네임} 으로 작성 */
+
+        <button onClick={increment}>{clickString} {count}</button>
     );
 };
 

cd4475474bdec94fba989d9713cb8cb929bbdb50
ejimDev <ejim0093@gmail.com>
2023-07-27 11:08:18 +0900

    Router 적용 및 Switch, Link 활용

--- study/src/App.js
+++ study/src/App.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import Movie from "./components/Movie";
 import MovieForm from "./components/MovieForm";
-import movie from "./components/Movie";
 import Navbar from "./components/Navbar";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 
 function App() {
     const [movies, setMovies] = useState([]);
@@ -12,7 +16,6 @@ function App() {
             return movie.id != id;
         }));
     };
-    /*filter를 이용하여 삭제 선택한 id가 아닌것만 출력하도록 함*/
 
     const rederingMovies = movies.length ? movies.map(movie =>{
        return (
@@ -32,13 +35,29 @@ function App() {
     };
 
     return (
-    <div className="App">
-        <Navbar />
-        <MovieForm addMovie={addMovie} />
-        <br />
-        <br />
-        {rederingMovies}
-    </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Switch> {/*Switch를 사용하면 코드를 내려가면서 가장 먼저 매치되는 Route가 뜸. 따라서 순서가 아주 중요함
+                만약 "/"으로 지정된 Route에서 exact옵션을 넣지않았다면 "users" 메뉴를 눌렀을때 "Home"이 떴을것임)*/}
+                    <Route path="/movies">
+                        <h1>Movie list</h1>
+                        <MovieForm addMovie={addMovie} />
+                        {rederingMovies}
+                    </Route> {/*라우터 밑에서 라우트를 사용할 수 있음. 라우트 하나당 하나의 페이지*/}
+                    <Route path="/" exact>
+                        {/* Switch를 쓰지않으면 슬래시 단위로 똑같을때 모두 뜸.
+                    예를들어 path="/"에서 출력되어야 할 부분이 "/movie"와 "/users"에서도 뜸
+                    (슬래시 단위기 때문에 "/user"로 지정해서 user라는 단어가 같더라도 "/users" 에서는 안뜸)
+                    또는 exact 옵션을 넣으면 정확히 일치할때만 콘텐츠를 보여줌*/}
+                        <h1>Home</h1>
+                    </Route>
+                    <Route path="/users">
+                        <h1>Users</h1>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
   );
 }
 
--- study/src/components/Navbar.js
+++ study/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Home</a>
+                <Link className="navbar-brand" to="/">Home</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -12,10 +13,10 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Movies</a>
+                            <Link className="nav-link" to="/movies">Movies</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Users</a>
+                            <Link className="nav-link" to="/users">Users</Link>
                         </li>
                     </ul>
                 </div>

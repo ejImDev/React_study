195397bd732897447b0da362b383e538097b7c5e
ejimDev <ejim0093@gmail.com>
2023-07-28 14:34:33 +0900

    배열값 불러오기전 대기스핀 기능 추가

--- study/src/components/pages/Users.js
+++ study/src/components/pages/Users.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import UserList from "./UserList";
+import Spinner from "../Spinner";
 
 const Users = () => {
     const [users, setUsers] = new useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
-                setUsers(response.data); /*axios를 통해서 응답값을 받고 그 데이터를 setUser로 반영*/
+                setUsers(response.data);
+                setLoading(false); /*리스트를 셋팅할때 false로 바꿔줌. 다시말해 리스트가 없는 초기상태만 true*/
             });
     }, []);
 
@@ -19,7 +22,8 @@ const Users = () => {
     return (
         <>
             <h1>Users</h1>
-            <UserList users={users}/>
+            {loading ? <Spinner /> : <UserList users={users}/> }
+            {/*loading이 true일때 == 리스트배열을 아직 가져오기전에만 로딩이 뜨도록 함*/}
         </>
   );
 };
